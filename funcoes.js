3ebf9ef4d004b9df3d3fee123e85a71d1f5cc6a6
Felipe Yukio Fupunaga <felipeyukio182@gmail.com>
2021-07-02 13:09:55 -0300

    Ajustado a probabilidade das lendarias, e adicionado o input informado pelo usuario nas opções de lendarias

--- funcoes.js
+++ funcoes.js
@@ -2,7 +2,8 @@
 function roletar() {
     const resLendario = Math.ceil(Math.random() * chancesLendario)
     if (resLendario == 1) {
-        resultado.innerHTML += "Lendario || "
+        
+        resultado.innerHTML += `<strong>${sortearLendario(garantidoLendario)}</strong> || `
         chancesLendario = 90
     } else {
         //errou lendario
@@ -14,6 +15,24 @@ function roletar() {
         } else {
             resultado.innerHTML += "Comum || "
             chancesEpico--
+        }
+    }
+}
+
+function sortearLendario(garantido) {
+    if (garantido) {
+        garantidoLendario = false
+
+        return bannerLendario.value
+
+    } else {
+        const cLendario = Math.ceil(Math.random() * 2)
+        if (cLendario == 1) {
+            return bannerLendario.value
+        } else {
+            garantidoLendario = true
+            const qualLendario = Math.ceil(Math.random() * listagemLendarios.length)
+            return listagemLendarios[qualLendario - 1]
         }
     }
 }
